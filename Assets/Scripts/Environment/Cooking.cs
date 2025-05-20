8dff766e05487c30a53347d44cfbccbfce10e1e0
Grolsch1 <chrisbeckenstrater@gmail.com>
2025-05-20 19:40:15 +0200

    Revert "everything"
    This reverts commit 3643a6f53085a077456c29f3245b800373b0f01c.

--- Assets/Scripts/Environment/Cooking.cs
+++ Assets/Scripts/Environment/Cooking.cs
@@ -1,86 +1,52 @@
 using UnityEngine;
-using System.Collections;
 
 public class Cooking : MonoBehaviour
 {
-    bool cooked = false;
+    bool cooked =  false;
     Player player;
     PlayerInventory inventory;
     PlayerData data;
     public float newMaxHealth = 30f;
     bool inRainge = false;
-    /*public GameObject cookobj;
-    public GameObject recepieScreen;
-    public GameObject cookingScreen;
-    public GameObject DoneScreen;*/
-
+    
     void Start()
     {
         player = GameObject.FindWithTag("Player").GetComponent<Player>();
         data = GameObject.FindWithTag("PlayerData").GetComponent<PlayerData>();
         inventory = GameObject.FindWithTag("Player").GetComponent<PlayerInventory>();
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (inRainge)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                //CookRecipe(1);
-                if (!cooked)
-                {
-                    print("health up");
-        player.SetMaxHealth(newMaxHealth);
-        player.HealthFull();/*
+        if(inRainge){
+            if (Input.GetKeyDown(KeyCode.E)){ 
+                if(!cooked){
                     cooked = true;
-                    cookobj.SetActive(true);
-                    recepieScreen.SetActive(true);
-                    cookingScreen.SetActive(true);
-                    */
+                    print("health up");
+                    player.SetMaxHealth(newMaxHealth);
+                    player.HealthFull();
                 }
             }
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.tag == "Player"){
             print("in");
             inRainge = true;
         }
     }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
+    private void OnTriggerExit2D(Collider2D collision){
+        if (collision.gameObject.tag == "Player"){
             inRainge = false;
         }
     }
-    /*
-    public void CookRecipe(int recipeIndex)
-    {
-        if (inventory.Contains(recipeIndex) && !data.GetUnlockedRecipe(recipeIndex))
-        {
-            cooked = true;
+    public void CookRecipe(int recipeIndex){
+        if (inventory.Contains(recipeIndex)){
             inventory.Reduce(recipeIndex);
-            data.SetUnlockedRecipe(recipeIndex, true);
+            data.SetUnlockedRecipe(recipeIndex,true);
         }
-
-    }
-    public void startCook(){
-        StartCoroutine(Cook());
-    }
-    IEnumerator Cook()
-    {
-        recepieScreen.SetActive(false);
-        yield return new WaitForSeconds(1f);
-        cookingScreen.SetActive(false);
-        yield return new WaitForSeconds(1f);
-        DoneScreen.SetActive(false);
-        cookobj.SetActive(true);
         
-    }*/
+    }
 }
